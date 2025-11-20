58d581373b311c4ef809f16cca9230f85220f666
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 16:05:16 -0300

    feat: cria controller para criar uma nova conversa caso não exista, caso exista ela retorna a existente do banco de dados

---
+++ src/main/java/com/lifty/sistemachat/api/controllers/ConversaController.java
@@ -0,0 +1,29 @@
+package com.lifty.sistemachat.api.controllers;
+
+import com.lifty.sistemachat.api.dto.conversa.ConversaRequestDTO;
+import com.lifty.sistemachat.api.dto.conversa.ConversaResponseDTO;
+import com.lifty.sistemachat.domain.service.ConversaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/conversas")
+public class ConversaController {
+
+    private final ConversaService conversaService;
+
+    public ConversaController(ConversaService conversaService) {
+        this.conversaService = conversaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<ConversaResponseDTO> criarConversa(@RequestBody @Valid ConversaRequestDTO conversaRequestDTO) {
+        ConversaResponseDTO conversaResponseDTO = conversaService.criarConversa(conversaRequestDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(conversaResponseDTO);
+    }
+}
