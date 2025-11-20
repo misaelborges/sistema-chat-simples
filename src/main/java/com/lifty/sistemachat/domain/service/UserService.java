ae345b3fb6e260ae3b6cc756232e823321c84ed9
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 11:18:05 -0300

    feat: adiciona a dependencia de UserMapper e cria o metodo salvarUsuario

--- src/main/java/com/lifty/sistemachat/domain/service/UserService.java
+++ src/main/java/com/lifty/sistemachat/domain/service/UserService.java
@@ -1,5 +1,9 @@
 package com.lifty.sistemachat.domain.service;
 
+import com.lifty.sistemachat.api.dto.user.UserRequestDTO;
+import com.lifty.sistemachat.api.dto.user.UserResponseDTO;
+import com.lifty.sistemachat.core.openapi.mapper.UserMapper;
+import com.lifty.sistemachat.domain.model.User;
 import com.lifty.sistemachat.domain.repositorie.UserRepository;
 import org.springframework.stereotype.Service;
 
@@ -7,8 +11,16 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
         this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserResponseDTO salvarUsuario(UserRequestDTO usuarioRequestDTO) {
+        User user = userMapper.toEntity(usuarioRequestDTO);
+        user = userRepository.save(user);
+        return userMapper.toUserResponseDTO(user);
     }
 }
