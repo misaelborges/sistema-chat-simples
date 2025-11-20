6fbffd48081107488e13fe2561028d2bd4adce98
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 16:04:47 -0300

    feat: cria metodo para criar uma nova conversa caso não exista, caso exista ela retorna a existente do banco de dados

--- src/main/java/com/lifty/sistemachat/domain/service/ConversaService.java
+++ src/main/java/com/lifty/sistemachat/domain/service/ConversaService.java
@@ -1,14 +1,49 @@
 package com.lifty.sistemachat.domain.service;
 
+import com.lifty.sistemachat.api.dto.conversa.ConversaRequestDTO;
+import com.lifty.sistemachat.api.dto.conversa.ConversaResponseDTO;
+import com.lifty.sistemachat.core.mapper.ConversaMapper;
+import com.lifty.sistemachat.domain.model.Conversa;
+import com.lifty.sistemachat.domain.model.User;
 import com.lifty.sistemachat.domain.repositorie.ConversaRepository;
+import com.lifty.sistemachat.domain.repositorie.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ConversaService {
 
     private final ConversaRepository conversaRepository;
+    private final UserRepository userRepository;
+    private final ConversaMapper conversaMapper;
 
-    public ConversaService(ConversaRepository conversaRepository) {
+    public ConversaService(ConversaRepository conversaRepository, ConversaMapper conversaMapper, UserRepository userRepository) {
         this.conversaRepository = conversaRepository;
+        this.conversaMapper = conversaMapper;
+        this.userRepository = userRepository;
+    }
+
+    public ConversaResponseDTO criarConversa(ConversaRequestDTO conversaRequestDTO) {
+        Conversa conversa;
+        User remetente = buscaUser(conversaRequestDTO.destinatario());
+        User destinatario = buscaUser(conversaRequestDTO.remetente());
+
+        Optional<Conversa> conversaUsuarios = conversaRepository.findConversaEntreUsuarios(remetente.getId(), destinatario.getId());
+
+        if (conversaUsuarios.isEmpty()) {
+            conversa = new Conversa();
+            conversa.setRemetente(remetente);
+            conversa.setDestinatario(destinatario);
+            conversa = conversaRepository.save(conversa);
+            return conversaMapper.toResponseDTO(conversa);
+        }
+
+        return conversaMapper.toResponseDTO(conversaUsuarios.get());
+    }
+
+    private User buscaUser(Long id) {
+        return userRepository.findById(id).orElseThrow(
+                () -> new RuntimeException(String.format("Usuario com %d não existe", id)));
     }
 }
