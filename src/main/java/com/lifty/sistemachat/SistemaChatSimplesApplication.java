9d56a673ef481b73aa6acc20f76e86ccb3ae184b
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 09:20:06 -0300

    git commit -m "first commit"

---
+++ src/main/java/com/lifty/sistemachat/SistemaChatSimplesApplication.java
@@ -0,0 +1,13 @@
+package com.lifty.sistemachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaChatSimplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaChatSimplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifty/sistemachat/SistemaChatSimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifty.sistemachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaChatSimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
