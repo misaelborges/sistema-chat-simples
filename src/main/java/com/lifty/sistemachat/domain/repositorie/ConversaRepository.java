22dc52c4222e0f53d1ec21048853e79f165684f6
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 15:53:49 -0300

    feat: cria metodo para buscar conversas entre usuarios dentor do banco de dados

--- src/main/java/com/lifty/sistemachat/domain/repositorie/ConversaRepository.java
+++ src/main/java/com/lifty/sistemachat/domain/repositorie/ConversaRepository.java
@@ -2,9 +2,22 @@ package com.lifty.sistemachat.domain.repositorie;
 
 import com.lifty.sistemachat.domain.model.Conversa;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ConversaRepository extends JpaRepository<Conversa, Long> {
+
+    @Query("""
+            SELECT c 
+            FROM Conversa c
+            WHERE (c.remetente.id = :idUsuario1 AND c.destinatario.id = :idUsuario2)
+               OR (c.remetente.id = :idUsuario2 AND c.destinatario.id = :idUsuario1)
+            """)
+    Optional<Conversa> findConversaEntreUsuarios(@Param("idUsuario1") Long idUsuario1,
+                                                 @Param("idUsuario2") Long idUsuario2);
     
 }
