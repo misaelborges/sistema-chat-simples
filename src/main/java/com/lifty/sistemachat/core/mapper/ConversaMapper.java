0250ab9a9ed1e864f857d32d46e2406cc06985e6
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 16:41:12 -0300

    refactor: refatora o metodo toResponseDTO para receber o UserResponseDTO

--- src/main/java/com/lifty/sistemachat/core/mapper/ConversaMapper.java
+++ src/main/java/com/lifty/sistemachat/core/mapper/ConversaMapper.java
@@ -1,6 +1,9 @@
 package com.lifty.sistemachat.core.mapper;
 
 import com.lifty.sistemachat.api.dto.conversa.ConversaResponseDTO;
+import com.lifty.sistemachat.api.dto.conversa.ConversaResponseResumoDTO;
+import com.lifty.sistemachat.api.dto.user.UserResponseDTO;
+import com.lifty.sistemachat.api.dto.user.UserResponseResumoDTO;
 import com.lifty.sistemachat.domain.model.Conversa;
 import org.springframework.stereotype.Component;
 
@@ -10,8 +13,8 @@ public class ConversaMapper {
     public ConversaResponseDTO toResponseDTO(Conversa conversa) {
         return new ConversaResponseDTO(
                 conversa.getId(),
-                conversa.getRemetente(),
-                conversa.getDestinatario(),
+                new UserResponseDTO(conversa.getRemetente().getId(), conversa.getRemetente().getNome()),
+                new UserResponseDTO(conversa.getDestinatario().getId(), conversa.getDestinatario().getNome()),
                 conversa.getMensagems(),
                 conversa.getCriadaEm());
     }
