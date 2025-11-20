4af2141e265517730263b756438b908fd1965aca
Misael Borges <mizaelborges2011@gmail.com>
2025-11-20 09:44:28 -0300

    refactor: adiciona getters, setters, construtores e equals/hashCode

--- src/main/java/com/lifty/sistemachat/domain/model/User.java
+++ src/main/java/com/lifty/sistemachat/domain/model/User.java
@@ -2,6 +2,8 @@ package com.lifty.sistemachat.domain.model;
 
 import jakarta.persistence.*;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "tbl_user")
 public class User {
@@ -12,4 +14,40 @@ public class User {
 
     @Column(nullable = false, length = 50)
     private String nome;
+
+    public User() {
+    }
+
+    public User(Long id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
