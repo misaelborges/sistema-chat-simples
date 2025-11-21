6b61e4d1f53c2ac68faea6d29367d8f97ee88810
Misael Borges <mizaelborges2011@gmail.com>
2025-11-21 12:47:43 -0300

    refactor: retira a propriedade mensagems, esta puluindo o payload

--- src/main/java/com/lifty/sistemachat/api/dto/conversa/ConversaResponseDTO.java
+++ src/main/java/com/lifty/sistemachat/api/dto/conversa/ConversaResponseDTO.java
@@ -1,16 +1,13 @@
 package com.lifty.sistemachat.api.dto.conversa;
 
 import com.lifty.sistemachat.api.dto.user.UserResponseDTO;
-import com.lifty.sistemachat.domain.model.Mensagem;
 
 import java.time.LocalTime;
-import java.util.List;
 
 public record ConversaResponseDTO(
         Long id,
         UserResponseDTO remetente,
         UserResponseDTO destinatario,
-        List<Mensagem> mensagems,
         LocalTime criadaEm
 ) {
 }
