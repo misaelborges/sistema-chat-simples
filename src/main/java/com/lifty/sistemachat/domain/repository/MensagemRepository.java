07346a3bb4ee551bcb9252204ef80d7fc8bfa5a1
Misael Borges <mizaelborges2011@gmail.com>
2025-11-21 12:48:43 -0300

    feat: adiciona a o metodo findByConversaIdOrderByDataHoraAsc para buscar uma conversa por id e ordena por data e hora

--- src/main/java/com/lifty/sistemachat/domain/repository/MensagemRepository.java
+++ src/main/java/com/lifty/sistemachat/domain/repository/MensagemRepository.java
@@ -1,10 +1,15 @@
-package com.lifty.sistemachat.domain.repositorie;
+package com.lifty.sistemachat.domain.repository;
 
 import com.lifty.sistemachat.domain.model.Mensagem;
+import com.lifty.sistemachat.domain.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MensagemRepository extends JpaRepository<Mensagem, Long> {
 
+    List<Mensagem> findByConversaIdOrderByDataHoraAsc(Long conversaId);
+    User findRemetenteById(Long remetenteid);
 }
